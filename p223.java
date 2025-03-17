28049c5591147e16e50d6d55e87e329ab8b794c9
FranPaton <52471823+FranPaton@users.noreply.github.com>
2025-03-17 23:30:44 +0100

    actulizalo el ejercicio p223

--- p223.java
+++ p223.java
@@ -9,9 +9,25 @@ public class p223 {
 			numeros[i] = in.nextInt();
 		}
 		
-		
-		int limiteIzq = 0, maxMulti = 0;
-		for(int i=0; i<limite; i++) {
+		int limiteIzq = 0, limiteIzqMax = -1, maxMulti = 0, contMulti = 0;
+		for(int i=0; i<limite;i++) {
+			if(numeros[i] == 0) {
+				contMulti = i - limiteIzq;
+				if(contMulti > maxMulti){
+					maxMulti = contMulti;
+					limiteIzqMax = limiteIzq;
+				}
+				limiteIzq = i + 1;
+			}
+		}
+		// Controlar la ultima secuencia despues del 0, o toda la secuencia si no hay 0
+		contMulti = limite - limiteIzq;
+		if(contMulti > maxMulti) {
+			maxMulti = contMulti;
+			limiteIzqMax = limiteIzq;
+		}
+				
+	/*	for(int i=0; i<limite; i++) {
 			for(int j=i; j<limite; j++) {
 				boolean ceroEncontrado = false;
 				for(int k=i; k<=j; k++) {
@@ -28,16 +44,15 @@ public class p223 {
 					}			
 				}
 			}
-		}
-		int limiteDer = limiteIzq + maxMulti - 1;
-		if(maxMulti > 0 ) {			
-			System.out.println(maxMulti + " -> " + "[" + limiteIzq + "," + limiteDer + "]");
+		}*/
+		
+		if(maxMulti > 0 ) {		
+			int limiteDer = limiteIzqMax + maxMulti - 1;
+			System.out.println(maxMulti + " -> " + "[" + limiteIzqMax + "," + limiteDer + "]");
 		} else {
 			System.out.println("SIGUE BUSCANDO");
-		}
-			
+		}	
 		// TU CÓDIGO AQUÍ
-
 	} // casoDePrueba
 
 	public static void main(String[] args) {
